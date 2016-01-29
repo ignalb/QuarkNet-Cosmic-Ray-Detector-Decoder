1969f945a8a16342f7d328a2adc5f24e644a378f
Ignacio Pacheco <baxter.ignalb@gmail.com>
2016-01-29 17:22:16 -0500

    Major overhaul to DataController.java
    Implemented full read-write functionality

--- src/main/java/DataController.java
+++ src/main/java/DataController.java
@@ -2,6 +2,11 @@ package main.java;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.time.LocalDateTime;
 import java.util.Scanner;
 
 public class DataController {
@@ -10,26 +15,47 @@ public class DataController {
 	private static File dat;
 	private static String date;
 	private static int count;
+	private FileWriter write;
 	
-	public DataController(){
+	public DataController() throws IOException{
 		Scanner in = new Scanner(System.in);
 		boolean isValid = false;
+		
 		do{
+			dat = new File(dat_PATH + "info.dat");
 			try {
 				in = new Scanner(dat);
 				isValid = true;
 			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-				dat = new File(dat_PATH + "info.dat");
-				//TODO Init file
+				date = DataParser.timeToStringDate(Files.readAttributes(dat.toPath(), BasicFileAttributes.class).lastModifiedTime());
+				count = 0;
+				update();
 			}
 		} while(!isValid);
-		date = in.next();	//TODO change to pattern equivalent to "yyyy-mm-dd" or just change to nextLine()
 		
+		//Reads file and updates variables
+		date = in.nextLine();
+		count = in.nextInt();
+		
+		in.close();
 	}
 	
-	public static int getCount(){
+	public int getCount() throws IOException{
+		String currentDate = DataParser.timeToStringDate(LocalDateTime.now().toString());
+		count = (date.equals(currentDate)) ? ++count : 1;
+		update();
 		return count;
+	}
+	
+	private void update(){
+		try{
+			write = new FileWriter(dat);
+			write.write(date + "\n");
+			write.write(count + "\n");
+			write.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
