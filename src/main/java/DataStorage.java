f9182119cba918b6fd1bbfb6174a564fa4b27825
Ignacio Pacheco <baxter.ignalb@gmail.com>
2016-02-01 14:12:57 -0500

    Created DataStorage.java
    Should keep a log of all data using the currently unimplemented classes
    DisplayDS and DisplayST

---
+++ src/main/java/DataStorage.java
@@ -0,0 +1,45 @@
+package main.java;
+
+import java.util.ArrayList;
+
+public class DataStorage {
+
+	private ArrayList<DisplayST> st = new ArrayList<DisplayST>();
+	private ArrayList<DisplayDS> ds = new ArrayList<DisplayDS>();
+	private DataType type;
+	private int deltaT;		//in minutes
+	private long tTime = 0;	//elapsed time (in minutes)
+	
+	public DataStorage(DataType type, int dt){
+		this.type = type;
+		deltaT = dt;
+	}
+	
+	public void addST(DisplayST st){
+		this.st.add(st);
+		tTime += deltaT;
+	}
+	public void addDS(DisplayDS ds){
+		this.ds.add(ds);
+	}
+	
+	public ArrayList<DisplayST> getSTs(){
+		return st;
+	}
+	public ArrayList<DisplayDS> getDSs(){
+		return ds;
+	}
+	public int getDeltaTime(){
+		return deltaT;
+	}
+	public long getTime(){
+		return tTime;
+	}
+	public DataType getSTtype(){
+		return type;
+	}
+}
+
+enum DataType {
+	ST1, ST2, ST3
+}
