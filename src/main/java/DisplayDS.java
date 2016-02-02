41522e360f95227cb2e1adb9c443b750b54be6f0
Ignacio Pacheco <baxter.ignalb@gmail.com>
2016-02-02 14:47:09 -0500

    Overhaul to DisplayDS to match DisplayST

--- src/main/java/DisplayDS.java
+++ src/main/java/DisplayDS.java
@@ -5,19 +5,40 @@ public class DisplayDS {
 	private int[] chCount = new int[4];
 	private int trigCount;
 	
-	public DisplayDS(String ch0_16, String ch1_16, String ch2_16, String ch3_16, String trig_16){
-		String[] chHex = { ch0_16, ch1_16, ch2_16, ch3_16 };
-		for(int i = 0; i < chCount.length; i++){
-			chCount[i] = Integer.parseInt(chHex[i], 16);
-		}
-		trigCount = Integer.parseInt(trig_16, 16);
-	}
 	public DisplayDS(int ch0, int ch1, int ch2, int ch3, int trig){
 		int[] ch = { ch0, ch1, ch2, ch3 };
 		for(int i = 0; i < chCount.length; i++){
 			chCount[i] = ch[i];
 		}
 		trigCount = trig;
+	}
+	
+	public static DisplayDS parseDS(String s){
+		int[] ch = { 0, 0, 0, 0 };
+		int trig = 0;
+		
+		while(s.substring(0, 3).equals("DS\n") || s.substring(0, 3).equals("DS ")){
+			s = s.substring(2, s.length()).trim();
+		}
+		while(s.substring(0,  2).equals("DS")){
+			s = s.substring(1, s.length()).trim();
+		}
+		
+		for(int i = 0; i < 5; i++){
+			String parser = Util.nextWord(s);
+			s = s.substring(parser.length(), s.length()).trim();
+			
+			char[] leading = parser.substring(0, 3).toCharArray();
+			if(leading[0] == 'C' && Character.isDigit(leading[1]) && leading[2] == '=')
+				parser = parser.substring(3, parser.length());
+			
+			if(i < 4)
+				ch[i] = Integer.parseInt(parser, 16);
+			else
+				trig = Integer.parseInt(parser, 16);
+		}
+		
+		return new DisplayDS(ch[0], ch[1], ch[2], ch[3], trig);
 	}
 	
 	public int[] getCounts(){
